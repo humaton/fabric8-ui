4d2548ba2da941d396fc1bda5e08789813de5d58
Jaivardhan <jaivardhan@dhcp35-152.lab.eng.blr.redhat.com>
2017-02-07 12:33:07 +0530

    datatable for showing components

--- src/app/analyze/stackreports/render-stack-details/module.ts
+++ src/app/analyze/stackreports/render-stack-details/module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import {DataTableModule} from "angular2-datatable";
 //import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import { RenderStackDetailsComponent } from './render-stack-details.component';
 import { StackAnalysesService } from '../stack-analyses.service';
@@ -16,7 +17,7 @@ import {ContainerTogglerModule} from '../container-toggler/module';
 import { CommonModule }     from '@angular/common';
 
 @NgModule({
-  imports: [StackRecoModule, ContainerTogglerModule],
+  imports: [StackRecoModule, ContainerTogglerModule, CommonModule, DataTableModule],
   declarations: [ RenderStackDetailsComponent ],
   schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
